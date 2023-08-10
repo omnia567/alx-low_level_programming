9c6837633374f7eee2fd60b24469381abb1c98e3
Omnia <Omniaali073@gmail.com>
2023-08-10 15:46:36 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  * Description: A C program that prints with puts function.
  *
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
-    return (0);
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
