1581ad1b04dfc8c24c72c87669d7668b8a643b21
Omnia <Omniaali073@gmail.com>
2023-08-30 11:24:51 -0700

    hhhh

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * _puts_recursion - Prints a string followed by a new line
  * @s: string
  *
  * Return: On success 1.
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
