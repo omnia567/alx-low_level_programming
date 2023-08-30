1e53655e582a4d8c33caf56156c3515851826d3d
Omnia <Omniaali073@gmail.com>
2023-08-30 11:28:15 -0700

    hhhh

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,23 @@
 #include "main.h"
+
 /**
  * _puts_recursion - Prints a string followed by a new line
  * @s: string
  *
  * Return: On success 1.
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+
+ if (*s == '\0')
+ {
+  _putchar('\n');
+ }
+ else
+ {
+  _putchar(*s);
+  _puts_recursion(s + 1);
+ }
+
 }
