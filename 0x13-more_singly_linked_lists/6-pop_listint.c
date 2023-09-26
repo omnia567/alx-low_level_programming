4d088ad64d5e791a4d28624df594bffec1d1df57
Omnia <Omniaali073@gmail.com>
2023-09-26 11:21:56 -0700

    i

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,16 +9,16 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *temp;
-	int num;
+	listint_t *tempp;
+	int number;
 
 	if (!head || !*head)
 		return (0);
 
-	num = (*head)->n;
-	temp = (*head)->next;
+	number = (*head)->n;
+	tempp = (*head)->next;
 	free(*head);
-	*head = temp;
+	*head = tempp;
 
-	return (num);
+	return (number);
 }
