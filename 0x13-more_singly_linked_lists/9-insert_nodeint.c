b8cf2657d8a61b222bb47731b69d69fc1fcb471e
Omnia <Omniaali073@gmail.com>
2023-09-26 11:28:03 -0700

    u

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *tempp = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (!new || !head)
@@ -29,16 +29,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	for (i = 0; tempp && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
-			new->next = temp->next;
-			temp->next = new;
+			new->next = tempp->next;
+			tempp->next = new;
 			return (new);
 		}
 		else
-			temp = temp->next;
+			tempp = tempp->next;
 	}
 
 	return (NULL);
